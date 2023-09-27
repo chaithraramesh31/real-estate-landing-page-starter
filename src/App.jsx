3ddb990e623d8f66be035532ad009b589eb38f9a
Chaithra R <chaithraramesh31@gmail.com>
2023-09-27 17:03:56 +0530

    added companies and guide section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import Hero from "./Components/Hero";
+import Companies from "./Components/Companies";
+import Guide from "./Components/Guide";
 
 function App() {
   return (
     <>
       <Hero/>
+      <Companies/>
+      <Guide/>
     </>
   );
 }
